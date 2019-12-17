8077b83274ca2eb82ba068acc9861cda5891b159
evettetelyas <etelyas@gmail.com>
2019-12-16 21:15:49 -0700

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/symptoms', to: 'symptoms#index'
+      resources :symptoms, only: [:index]
+      resources :conditions, only: [:index, :show]
       get '/symptoms/:location', to: 'symptoms#by_location'
-      get '/conditions', to: 'conditions#index'
-      get '/conditions/:id', to: 'conditions#show'
     end
   end
 end
