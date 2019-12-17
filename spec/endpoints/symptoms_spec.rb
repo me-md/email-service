260d1cfb59181500aa91d8558f57d7c8e456062a
evettetelyas <etelyas@gmail.com>
2019-12-16 21:09:59 -0700

    update symptom spec for search by location

--- spec/endpoints/symptoms_spec.rb
+++ spec/endpoints/symptoms_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Symptoms Endpoint", type: :request do
                   sex_filter: 'both', 
                   category: 'Signs and symptoms', 
                   seriousness: 'emergency', 
-                  location: 'everywhere'
+                  location: 'Head'
                 )
     Symptom.create(sid: 'asdsadsa', 
                   name: 'moar pain', 
@@ -16,15 +16,15 @@ RSpec.describe "Symptoms Endpoint", type: :request do
                   sex_filter: 'both', 
                   category: 'Signs and symptoms', 
                   seriousness: 'emergency', 
-                  location: 'everywhere'
+                  location: 'Neck'
                 )
-    Symptom.create(sid: 'adwgrgwsefs', 
+    @symp_3 = Symptom.create(sid: 'adwgrgwsefs', 
                   name: 'EXTREME pain', 
                   common_name: 'kill me', 
                   sex_filter: 'both', 
                   category: 'Signs and symptoms', 
                   seriousness: 'emergency', 
-                  location: 'everywhere'
+                  location: 'Face'
                 )
 	end
 	
@@ -34,5 +34,15 @@ RSpec.describe "Symptoms Endpoint", type: :request do
     symptoms = JSON.parse(response.body)
 
 		expect(symptoms["data"].count).to eq(3)
+  end
+
+  it 'returns symptoms for a specific location' do
+    get '/api/v1/symptoms/face'
+    symptoms = JSON.parse(response.body)
+
+    expect(symptoms["data"][0]["attributes"]["name"]).to eq(@symp_3.name)
+    expect(symptoms["data"][0]["attributes"]["common_name"]).to eq(@symp_3.common_name)
+    expect(symptoms["data"][0]["attributes"]["sex_filter"]).to eq(@symp_3.sex_filter)
+    expect(symptoms["data"][0]["attributes"]["location"]).to eq(@symp_3.location)
   end
 end
